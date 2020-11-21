0ea1d70a88ee5d6d6b60326dd4ef58cf35c83f01
surestdeng <surestdeng@xiaoe-tech.com>
2020-11-21 22:10:42 +0800

    新增jwt获取和解析

--- pkg/auth/jwt.go
+++ pkg/auth/jwt.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"gocms/app/models/users"
 )
@@ -11,47 +12,32 @@ var (
 
 type JwtAction struct{}
 
-type UserClaims struct {
-	jwt.StandardClaims
-	authUser struct {
-		users.AuthUser
-	}
-}
-
 func init() {
-	key := "123455sign"
+	key := "AllYourBase"
 	signKey = []byte(key)
 }
 
-func (*JwtAction) GetToken(user users.AuthUser) string {
-	claims := &UserClaims{
-		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: 0,
-		},
-		authUser: struct {
-			users.AuthUser
-		}{user},
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+func (*JwtAction) GetToken(userClaims users.AuthUser) string {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, userClaims)
 	tokenString, _ := token.SignedString(signKey)
 	return tokenString
 }
 
 func (*JwtAction) ParseToken(tokenString string) (users.AuthUser, error) {
-	token, err := jwt.ParseWithClaims(tokenString, &UserClaims{}, func(token *jwt.Token) (i interface{}, e error) {
+
+	userClaims := users.AuthUser{}
+	token, err := jwt.ParseWithClaims(tokenString, &userClaims, func(token *jwt.Token) (i interface{}, e error) {
 		return signKey, e
 	})
 
-	user := users.AuthUser{}
-
 	if err != nil {
-		return user, err
+		return userClaims, err
 	}
 
-	if userClaims, ok := token.Claims.(*UserClaims); ok && token.Valid {
-		return userClaims.authUser.AuthUser, nil
+	if _, ok := token.Claims.(*users.AuthUser); ok && token.Valid {
+		fmt.Println(userClaims)
+		return userClaims, nil
 	} else {
-		return user, err
+		return userClaims, err
 	}
 }
