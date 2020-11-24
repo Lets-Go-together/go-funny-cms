7679ac1eb1b1e5d36a8b147057f21d8c39d8d864
surest <chenf@surest.cn>
2020-11-24 20:45:44 +0800

    新增登录验证

--- app/http/admin/controllers/auth.go
+++ app/http/admin/controllers/auth.go
@@ -1,11 +1,32 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	authValidate "gocms/app/validates/auth"
+	"gocms/pkg/response"
+	"net/http"
+)
 
-func Login(c *gin.Context) {
+type AuthController struct{}
 
+func (*AuthController) Login(c *gin.Context) {
+	authValidateAction := authValidate.LoginAction{}
+	if msg := authValidateAction.Validate(c); len(msg) > 0 {
+		c.JSON(http.StatusOK, response.JsonResponse{
+			Status:  403,
+			Message: msg,
+		})
+		return
+	}
+
+	//params := authValidateAction.GetLoginData()
+
+	c.JSON(http.StatusOK, response.JsonResponse{
+		Status:  200,
+		Message: "Success",
+	})
 }
 
-func Register() {
+func (*AuthController) Register(c *gin.Context) {
 
 }
