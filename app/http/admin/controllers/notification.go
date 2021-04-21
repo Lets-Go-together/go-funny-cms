f112096d5f07d9b2b3dffecb96ecb4655f4b8854
surest <chenf@surest.cn>
2021-04-21 22:27:22 +0800

    对接一下api

--- app/http/admin/controllers/notification.go
+++ app/http/admin/controllers/notification.go
@@ -5,30 +5,40 @@ import (
 	"github.com/spf13/cast"
 	"gocms/app/http/admin/validates"
 	"gocms/app/models/admin"
+	"gocms/app/models/base"
 	"gocms/app/models/notification"
 	"gocms/app/service"
 	"gocms/pkg/config"
 	"gocms/pkg/help"
 	"gocms/pkg/response"
 	"gocms/wrap"
+	"time"
 )
 
 type NotificationController struct{}
 
 func (n *NotificationController) List(c *wrap.ContextWrapper) {
-	tag := c.DefaultQuery("tag", 0)
+	title := c.DefaultQuery("title", "")
 	page := c.DefaultQuery("page", 1)
 	pageSize := c.DefaultQuery("pageSize", 10)
+	offset := help.GetOffset(cast.ToInt(page), cast.ToInt(pageSize))
 
 	model := &notification.Notification{}
 	models := []notification.Notification{}
 	query := config.Db.Model(&model)
-	if len(tag) > 0 {
-		query = query.Where("tag = ?", tag)
+	if len(title) > 0 {
+		query = query.Where("title like %?%", title)
 	}
-	query = query.Limit(pageSize).Offset(page).Scan(&models)
+	query = query.Limit(pageSize).Offset(offset).Scan(&models)
 
-	response.SuccessResponse(models).WriteTo(c)
+	data := base.Result{
+		Page:     cast.ToInt(page),
+		PageSize: cast.ToInt(pageSize),
+		List:     models,
+		Total:    0,
+	}
+
+	response.SuccessResponse(data).WriteTo(c)
 	return
 }
 
@@ -39,10 +49,9 @@ func (n *NotificationController) Store(c *wrap.ContextWrapper) {
 	description, _ := json.Marshal(map[string]string{"content": params.Description})
 	model := &notification.Notification{
 		Title:        params.Title,
-		Tag:          params.Tag,
 		Submitter_id: cast.ToInt(admin.AuthUser.Id),
 		Description:  string(description),
-		Read_at:      nil,
+		Read_at:      base.TimeAt(time.Now()),
 	}
 
 	follow_ids := []int{}
--- app/http/admin/validates/notification.go
+++ app/http/admin/validates/notification.go
@@ -2,7 +2,6 @@ package validates
 
 type NotificationSaveValidate struct {
 	Title        string `validate:"required" json:"title"`
-	Tag          int    `validate:"required" json:"tag"`
 	Submitter_id int    `json:"submitter_id"`
 	Description  string `validate:"required" json:"description"`
 	FollowIds    []int  `validate:"required" json:"fllow_ds"`
